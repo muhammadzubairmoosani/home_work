1ebecc38c20de09fdf194311ce0b2eccc5317e68
Zubair-SCT <zubairmoosani>
2022-10-11 03:00:43 +0500

    code refector

--- exercise_29.js
+++ exercise_29.js
@@ -1,16 +1,22 @@
 const favorite_fruits = ["apple", "pomegranate", "sugarcane"];
+
 favorite_fruits.map((fruit) => {
   if (fruit === "banana") {
-    console.log("You really like bananas!");
-  } else if (fruit === "mango") {
-    console.log("You really like mango!");
-  } else if (fruit === "apple") {
-    console.log("You really like apple!");
-  } else if (fruit === "grapes") {
-    console.log("You really like grapes!");
-  } else if (fruit === "strawberry") {
-    console.log("You really like strawberry!");
-  } else if (fruit === "pomegranate") {
-    console.log("You really like pomegranate!");
+    console.log(`You really like ${fruit}!`);
+  }
+  if (fruit === "mango") {
+    console.log(`You really like ${fruit}!`);
+  }
+  if (fruit === "apple") {
+    console.log(`You really like ${fruit}!`);
+  }
+  if (fruit === "grapes") {
+    console.log(`You really like ${fruit}!`);
+  }
+  if (fruit === "strawberry") {
+    console.log(`You really like ${fruit}!`);
+  }
+  if (fruit === "pomegranate") {
+    console.log(`You really like ${fruit}!`);
   }
 });
--- exercise_33.js
+++ exercise_33.js
@@ -1,7 +1,6 @@
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const ordinals = ["st", "nd", "rd", "th", "th", "th", "th", "th", "th"];
 
-const createOrdinalNumbers = (array) => {
-  return array.map((number) => `${number}th`);
-};
-
-console.log(createOrdinalNumbers(numbers));
+for (let i = 0; i < numbers.length; i++) {
+  console.log(`${numbers[i]}${ordinals[i]}`);
+}
